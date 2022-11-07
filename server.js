dd4574a6ac0d137397b2a118cbeb29811732920b
kayfernander2022 <kayfernander@hotmail.com>
2022-11-07 10:37:59 -0500

    Greeting express application created

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const app = express();
 
 
 
+app.get("/greeting/:name", (req, res) => {
+  //res.render('all.ejs', {fruits})
+  res.send('Hello ' + req.params.name + " Its so great to see you!")
+});
+
+
 
 
 app.listen(3001, () => {
-  console.log('Hey, listening on port 3000');
+  console.log('Hey, listening on port 3001');
 });
