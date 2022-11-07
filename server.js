d8e2df96e7e5928f90a9280520ea6fd894e7dfcd
kayfernander2022 <kayfernander@hotmail.com>
2022-11-07 13:17:09 -0500

    Tip calculator express application created

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const app = express();
 
 
 app.get("/greeting/:name", (req, res) => {
-  //res.render('all.ejs', {fruits})
+  
   res.send('Hello ' + req.params.name + " Its so great to see you!")
+});
+
+
+
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+const totalBill = parseInt(req.params.total); //100
+const tipPercentageIs = parseInt(req.params.tipPercentage); //20
+
+let sum = ((tipPercentageIs / 100)* totalBill)// 20/100
+  res.send (sum+"")
+  
 });
 
 
